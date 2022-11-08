a250e1d95def07691c63c618d98e370171e65f7d
Jim Bergstrom <jbergstrom@happybits.co>
2022-11-08 14:58:26 -0700

    Change out example for one with lighter requirements

--- example/source_code/hello_world.py
+++ example/source_code/hello_world.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*-
-import boto3
-import  PIL
-from AWSScout2.__main__ import main
+import requests
+
 def handler(event, context):
-    print("Lambda Module Example")
-if __name__ == '__main__':
-    handler(event=None, context=None)
+    response = requests.get("https://example.com/")
+    print(response.text)
